7e1b1c016b5ad03cc626a9c67e26e4403fe9721e
Robin Moalic <robin.moalic@yahoo.fr>
2022-03-13 11:35:39 +0100

    fix: overlapping

--- src/compress/lz77.rs
+++ src/compress/lz77.rs
@@ -1,6 +1,6 @@
 use std::collections::vec_deque::VecDeque;
 
-fn posl_substr(d: &[u8], w: &[u8]) -> Option<(usize, usize)> {
+fn posl_substr_overlap(d: &[u8], w: &[u8]) -> Option<(usize, usize)> {
     let mut pos: usize = 0;
     let mut l: usize = 0;
     let mut found: bool = false;
@@ -16,8 +16,8 @@ fn posl_substr(d: &[u8], w: &[u8]) -> Option<(usize, usize)> {
         if w[i] == d[0] {
             let mut j = 0;
             found = true;
-            while i + j < wlenght && j < dlenght {
-                if w[i + j] != d[j] {
+            while j < dlenght {
+                if w[i + (j % (wlenght - i))] != d[j] {
                     break;
                 }
                 j += 1;
@@ -49,7 +49,7 @@ fn compress(data: &[u8], window_size: usize) -> Vec<(usize, usize, u8)> {
         let c: u8;
 
         let sw = window.make_contiguous();
-        if let Some(mat) = posl_substr(d, sw) {
+        if let Some(mat) = posl_substr_overlap(d, sw) {
             start_of_match = mat.0;
             if mat.1 < d.len() {
                 lenght_of_match = mat.1;
@@ -115,28 +115,50 @@ fn test_compress_decompress() {
     assert_eq!(decompress(compress(b"AABCBBABC", 3), 3), b"AABCBBABC");
     assert_eq!(decompress(compress(b"Hello friend, Hello world!", 5), 5), b"Hello friend, Hello world!");
     assert_eq!(decompress(compress(b"Hello friend, Hello world!", 100), 100), b"Hello friend, Hello world!");
+    assert_eq!(decompress(compress(b"Blah blah blah blah blah!", 100), 100), b"Blah blah blah blah blah!");
+    assert_eq!(decompress(compress(b"Blah blah blah blah blah!", 5), 5), b"Blah blah blah blah blah!");
 }
 
 #[test]
-fn test_posl_substr() {
-    assert_eq!(posl_substr(&[1, 2], &[3, 1, 3, 1, 2]),
+fn test_compress_simple() {
+    assert_eq!(compress(b"AABCBBABC", 50),
+               vec!((0, 0, 65), (0, 1, 66), (0, 0, 67), (2, 1, 66), (1, 2, 67)));
+    assert_eq!(compress(b"Hello friend, Hello world!", 50),
+               vec!((0, 0, 72), (0, 0, 101), (0, 0, 108), (2, 1, 111), (0, 0, 32), (0, 0, 102), (0, 0, 114), (0, 0, 105), (1, 1, 110), (0, 0, 100), (0, 0, 44), (5, 1, 72), (1, 5, 119), (4, 1, 114), (2, 1, 100), (0, 0, 33)));
+}
+
+#[test]
+fn test_compress_overlapping() {
+    let c = compress(b"Blah blah blah blah blah!", 6);
+    assert_eq!(c, vec!((0, 0, 66), (0, 0, 108), (0, 0, 97), (0, 0, 104), (0, 0, 32), (0, 0, 98), (1, 18, 33)));
+    let c = compress(b"Blah blah blah blah blah!", 5);
+    assert_eq!(c, vec!((0, 0, 66), (0, 0, 108), (0, 0, 97), (0, 0, 104), (0, 0, 32), (0, 0, 98), (0, 18, 33)));
+
+}
+
+#[test]
+fn test_posl_substr_overlap() {
+    assert_eq!(posl_substr_overlap(&[1, 2], &[3, 1, 3, 1, 2]),
                Some((3, 2)));
 
-    assert_eq!(posl_substr(&[0, 1, 2], &[3, 1, 3, 1, 2]),
+    assert_eq!(posl_substr_overlap(&[0, 1, 2], &[3, 1, 3, 1, 2]),
                None);
 
-    assert_eq!(posl_substr(&[1, 2, 4], &[3, 1, 3, 1, 2]),
+    assert_eq!(posl_substr_overlap(&[1, 2, 4], &[3, 1, 3, 1, 2]),
                Some((3, 2)));
 
-    assert_eq!(posl_substr(&[3, 1], &[3, 1, 3, 1, 2]),
+    assert_eq!(posl_substr_overlap(&[3, 1], &[3, 1, 3, 1, 2]),
                Some((0, 2)));
 
-    assert_eq!(posl_substr(&[1, 1], &[3, 1, 3, 1, 2]),
+    assert_eq!(posl_substr_overlap(&[1, 1], &[3, 1, 3, 1, 2]),
                Some((1, 1)));
 
-    assert_eq!(posl_substr(&[], &[3, 1, 3, 1, 2]),
+    assert_eq!(posl_substr_overlap(&[], &[3, 1, 3, 1, 2]),
                None);
 
-    assert_eq!(posl_substr(&[1, 2], &[]),
+    assert_eq!(posl_substr_overlap(&[1, 2], &[]),
                None);
+
+    assert_eq!(posl_substr_overlap(&[1, 2, 1, 2], &[0, 1, 2]),
+               Some((1, 4)));
 }
